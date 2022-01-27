a2199b19231d55d53e4d79b0de972c82456c64ef
Daliya Benny <daliyaebenny@gmail.com>
2022-01-26 21:09:52 -0800

    Added the handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,22 +1,13 @@
 module.exports = {
-  format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
-  get_emoji: () => {
-    const randomNum = Math.random();
-
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
-    } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
+    // helper function to format the date
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+    // helper function to make comments plural if there is two or more comments
+    format_plural: (word, amount) => {
+        if (amount >= 2) {
+            return `${word}s`;
+        }
+        return word;
     }
-  },
 };
